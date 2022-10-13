a01072c049f722f21715b701d59b12e7003d85f8
Titilope Mogaji <titilopemogaji@gmail.com>
2022-10-13 12:18:01 -0700

    new5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
-*main - Main function
-*
-*
-*Return: return 0 everytime
-*/
+ * main - Entry
+ * Return: Always 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
