215bf06f655b5534ccf9d79110e707957363115d
Titilope Mogaji <titilopemogaji@gmail.com>
2022-10-13 02:35:32 -0700

    Put4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Printing
  *
- * Description: prints "\"Programming is like building a multilingual puzzle"
- *
- * Return: 0
+ * Return: Always 0 (Successful)
 */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
