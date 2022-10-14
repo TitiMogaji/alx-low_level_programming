1d3eb7f99aa044689f9d83f71d37182ea2660191
Titilope Mogaji <titilopemogaji@gmail.com>
2022-10-14 06:41:42 -0700

    new 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,13 +13,10 @@ int main(void)
 		putchar(n);
 		if (n != 57)
 		{
-			putchar(n);
-			if (n != 57)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
+	}
 	putchar('\n');
 	return (0);
 }
