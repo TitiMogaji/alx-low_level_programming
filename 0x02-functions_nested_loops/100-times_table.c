706d31b30e5572baf27ad100d7e7396c83d83422
Titilope Mogaji <titilopemogaji@gmail.com>
2022-10-18 15:53:16 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,23 +24,28 @@ void print_times_table(int n)
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
-				} else if (k > 9)
+				}
+				else if (k > 9)
 				{
 					_putchar(',');
 					_putchar(32);
 					_putchar(32);
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
-				} else if (j != 0)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(32);
 				}
-				_putchar(k + '0');
+				else
+				{
+					if (j != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(k + '0');
+				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
