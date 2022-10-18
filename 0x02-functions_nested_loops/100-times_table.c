5da10f70d49690d720b80512db4fad98bafbd526
Titilope Mogaji <titilopemogaji@gmail.com>
2022-10-18 15:43:04 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,46 +2,45 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
+ * @n: The number to be treated
+ *
+ * Return: Number matrix
  */
 void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n >= 0 && n <= 15)
+	if (n >= 0 && n <= 14)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
 				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
+				if (k > 99)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
+					_putchar(32);
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				} else if (k > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
+				} else if (j != 0)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(32);
 				}
+				_putchar(k + '0');
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
