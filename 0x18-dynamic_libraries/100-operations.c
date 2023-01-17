a128a14dcb661dcc0ccd6e5c9b33b3ff2995bde0
Titilope Mogaji <titilopemogaji@gmail.com>
2023-01-17 06:57:20 -0800

    commiter

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,50 +1,64 @@
 /**
- * add - addition
- * @a: number
- * @b: nuber
- * Return: result
- */
-int add(int a, int b)
+* add - Computes the sum of two numbers
+* @left: The left-sided number
+* @right: The right-sided number
+*
+* Return: Result
+*/
+
+int add(int left, int right)
 {
-      return (a + b);
+	return (left + right);
 }
+
 /**
- * sub - addition
- * @a: number
- * @b: nuber
- * Return: result
- */
-int sub(int a, int b)
+* sub - Computes the difference of two numbers
+* @left: The left-sided number
+* @right: The right-sided number
+*
+* Return: Result
+*/
+
+int sub(int left, int right)
 {
-      return (a - b);
+	return (left - right);
 }
+
 /**
- * mul - addition
- * @a: number
- * @b: nuber
- * Return: result
+ * mul - Computes the product of two numbers
+ * @left: The left-sided number
+ * @right: The right-sided number
+ *
+ * Return: Result
  */
-int mul(int a, int b)
+
+int mul(int left, int right)
 {
-      return (a * b);
+	return (left * right);
 }
+
 /**
- * div - addition
- * @a: number
- * @b: nuber
- * Return: result
+ * div - Computes the quotient of two numbers
+ * @left: The left-sided number
+ * @right: The right-sided number
+ *
+ * Return: Result
  */
-int div(int a, int b)
+
+int div(int left, int right)
 {
-      return (a / b);
+	return (left / right);
 }
+
 /**
- * mod - addition
- * @a: number
- * @b: nuber
- * Return: result
+ * mod - Computes the modulo of two numbers
+ * @left: The left-sided number
+ * @right: The right-sided number
+ *
+ * Return: Result
  */
-int mod(int a, int b)
+
+int mod(int left, int right)
 {
-      return (a % b);
+	return (left % right);
 }
